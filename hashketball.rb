c52c032e8b194c26eeeb82b4e9903d087cf2429f
Steven wu <stevenwu91@icloud.com>
2020-09-30 19:37:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -204,8 +204,8 @@ def player_stats(player_name)
   game_hash.each do |team, team_details_hash|
     team_details_hash[:players].each do |stats|
 
-      if stats[:name] == player_name
-        stats.delete(:name)
+      if stats[:player_name] == player_name
+        stats.delete(:player_name)
         player_stats = stats
       end
     end
